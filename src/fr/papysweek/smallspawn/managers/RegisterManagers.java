41e44b566f38f1a45ebc01e93cbc9dfb5e3ca0c4
PapySweek <38499601+PapySweek@users.noreply.github.com>
2020-07-10 23:34:26 +0200

    Update RegisterManagers.java

--- src/fr/papysweek/smallspawn/managers/RegisterManagers.java
+++ src/fr/papysweek/smallspawn/managers/RegisterManagers.java
@@ -3,6 +3,7 @@ package fr.papysweek.smallspawn.managers;
 import fr.papysweek.smallspawn.Main;
 import fr.papysweek.smallspawn.commands.SetSpawn;
 import fr.papysweek.smallspawn.commands.Spawn;
+import fr.papysweek.smallspawn.listeners.PlayerJoin;
 import fr.papysweek.smallspawn.listeners.PlayerRespawn;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.PluginManager;
@@ -14,6 +15,7 @@ public class RegisterManagers {
     public void register(){
         PluginManager pm = Bukkit.getPluginManager();
         pm.registerEvents(new PlayerRespawn(), instance);
+        pm.registerEvents(new PlayerJoin(), instance);
 
         instance.getCommand("spawn").setExecutor(new Spawn());
         instance.getCommand("setspawn").setExecutor(new SetSpawn());
